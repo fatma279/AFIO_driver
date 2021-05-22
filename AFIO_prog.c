5b805ed7556e500d9b4401413e3bf2eff4dcdbf3
fatma279 <60367290+fatma279@users.noreply.github.com>
2021-05-23 00:07:10 +0200

    Update AFIO_prog.c

--- AFIO_prog.c
+++ AFIO_prog.c
@@ -16,22 +16,27 @@
 	 /* LOC_u8RegIndex which register in array and the right position for line  */
 	 if(copy_u8Line < 4)
 	 {
+		 //AFIO_EXTICR1
 		LOC_u8RegIndex = 0;		
 	 }
 	 else if(copy_u8Line < 8)
-	 {
+	 {      
+		 //AFIO_EXTICR2
 		 LOC_u8RegIndex = 1;
+		 // to choose the right location in the registe 
 		 copy_u8Line -=4;
 	 }
 	 else if(copy_u8Line < 12)
-	 {
+	 { 
+		 //AFIO_EXTICR3
 		 LOC_u8RegIndex = 2;
-		 copy_u8Line -=4;
+		 copy_u8Line -=8;
 	 }
 	 else if(copy_u8Line < 16)
 	 {
+		 //AFIO_EXTICR4
 		 LOC_u8RegIndex = 3;
-		 copy_u8Line -=4;
+		 copy_u8Line -=12;
 	 }	 
 	 /* clear the line position before write its port*/
 	 AFIO->EXTICR[LOC_u8RegIndex] &= ~((0b1111)<< (copy_u8Line * 4));
